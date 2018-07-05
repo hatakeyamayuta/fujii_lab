005ddce241d334595d5a8cf3da5652875f390828
htk <s16c1101uq@s.chibakoudai.jp>
2018-07-05 21:35:42 +0900

    RANSAC?

--- subpix/template_matching_subpix.py
+++ subpix/template_matching_subpix.py
@@ -2,11 +2,19 @@ import numpy as np
 import cv2
 import sys
 import time
+from RANSAC import ransac
+from plot import plot_3d
 m = 16
 
-def RNSCAC():
-    pass
-
+def RANSAC(l,i,k):
+    F = np.array(([-4.27949302458962e-06,9.65167261094930e-06,-0.0349853780456422],
+                  [4.16571631974352e-05,-1.76320171777443e-05,0.386437272667093],
+                  [0.0235509488713194,-0.394326349874653,-12.6136322246641]))
+    t = np.array([i,l,1])
+    t2 = np.array([k,l,1])
+    r = np.dot(t2.T,F)
+    r = np.dot(r,t)
+    return -r
 
 def SSD(r_array,l_array,l,i,k,m):
     diff = abs(r_array[l:l+m,i:i+m] - l_array[l:l+m,k:k+m])
@@ -28,8 +36,8 @@ def ZNCC(l_array,r_array,l,i,k,m):
 def zncc_match():
     r_array,l_array,pos = read_stereo_img()
     y ,x = r_array.shape[:2]
-    for l in range(0,y-m,m):
-        for k in range(m,x-m,m):
+    for l in range(0,y-m,1):
+        for k in range(m,x-m,1):
             old = 0 
             for i in range(0,x-m):
                 if i==0 or i ==x-m:
@@ -38,7 +46,7 @@ def zncc_match():
                 eva1 = ZNCC(l_array,r_array,l,i+1,k,m)
                 eva_1 = ZNCC(l_array,r_array,l,i-1,k,m)
 
-                if eva >= 0.7 and k-i > 50 :
+                if eva >= 0.7 and k-i > 50:
                     if eva > old:
                         dist = k - i
                         pos[l:l+m, k:k+m] =  dist
@@ -47,27 +55,31 @@ def zncc_match():
     return pos
                
 def sad_match():
+    d = 0
     print("__start__","mask{0}".format(m))
     r_array,l_array,pos = read_stereo_img()
     y ,x = r_array.shape[:2]
-    for l in range(0,y-m,3):
-        for k in range(m,x-m,3):
+    for l in range(0,y-m,1):
+        for k in range(m,x-m,1):
             old =  2000
             for i in range(0,x-m):
-                if i==0 or i ==x-m:
+                if i == 0 or i ==  x-m:
                     continue
                 t = SSD(r_array, l_array,l,i,k,m)
                 if t < 2000:
                     if t < old:
                         R1 = SSD(r_array,l_array,l,i+1,k,m)
                         R_1 = SSD(r_array,l_array,l,i-1,k,m)
-                        if R1 < R_1:
-                            d = ((R1-R_1)/(t-R_1))*0.5
+                        #print(t,R1,R_1)
+                        if R1 <= R_1:
+                            d = ((R_1-R1)/(2*R_1-2*t))
                         else:
-                            d = ((R1-R_1)/(t-R1))*0.5
-                        dist = k - i + d
-                        pos[l:l+m, k:k+m] =  dist
-                        old = t
+                            d = -((R_1-R1)/(2*R1-2*t))
+                        if RANSAC(l,i,k) < 25:
+                            dist = k - i + d
+                            #pos[l:l+m, k:k+m] =  dist
+                            pos[l, k] =  dist
+                            old = t
 
 
     return pos
@@ -98,13 +110,12 @@ def template_match(key):
     t2 = time.time() -t1
     print("__Finish_time:{0}s__".format(t2))
 
-
+    #pos = ransac(pos)
     from PIL import Image
-
+    plot_3d(pos)
     Image.fromarray(np.uint8(pos)).save("stereo_gray.png")
-    with open("stereo_match.txt","w") as f:
-        f.write(pos)
-
+    
+    np.save("stereo_match",pos)
 
 
 
@@ -118,8 +129,6 @@ def main(key):
     else:
         print("__False_keyward__")
         exit()
-
-    
 
 
 if __name__=="__main__":
